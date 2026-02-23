6db0af881eb71b12559a3dc1f89fa30aa2f9b71e
Riddhima-gangwar <riddhima.gangwar9@gmail.com>
2026-02-23 09:10:23 +0530

    first commit

---
+++ Pattern.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Pattern {
+    public static void main(String[] args) {
+        // for (int i = 1; i <= 4; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("x ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+        // for(int i=1;i<=5;i++)
+        // {
+        //     for(int j=1;j<=i;j++)
+        //     {
+        //           System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+        // for (int i = 5; i >0; i--) {
+        //     for (int j = 1; j <= i; j++) {
+        //         System.out.print("x ");
+        //    }
+        //     System.out.println();
+        // }
+
+
+        int s = 1;
+    for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j <= i; j++) {
+        System.out.print(s+" ");
+        s++;
+    }
+    System.out.println();
+    }
+
+
+
+
+
+    }
+}
+
