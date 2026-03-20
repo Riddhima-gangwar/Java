eda7d73bc0b734723b22ff5eb53d3b35a5d43320
[Riddhima Gangwar] <riddhima.gangwar9@gmail.com>
2026-03-20 14:50:25 +0530

    message

---
+++ Thiskeyword.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Thiskeyword 
+{
+    int a;
+    void add(int a)
+    {
+        this.a = a;  
+    }
+    Thiskeyword()
+    {
+        this(1);  
+    }
+    Thiskeyword(int x)
+    {
+        System.out.println("Constructor 2");
+        a = x;
+    }
+    public static void main(String[] args) 
+    {
+        Thiskeyword obj = new Thiskeyword();
+        obj.add(10);
+        System.out.println(obj.a);
+    }
+}
