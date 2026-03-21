6b2561ea024a7a955b8fd53cf31a183da7e52fe3
[Riddhima Gangwar] <riddhima.gangwar9@gmail.com>
2026-03-21 15:33:03 +0530

    message

---
+++ Tern.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Tern {
+    public static void main(String[]args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int age=sc.nextInt();
+        boolean p=(age>18)?true:false;
+        System.out.println(p);
+    }
+}
