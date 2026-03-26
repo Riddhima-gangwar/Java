6d9c1ff29fbf700f1d88b7ed54efbb13a646345c
[Riddhima Gangwar] <riddhima.gangwar9@gmail.com>
2026-03-26 23:53:14 +0530

    message

---
+++ Superkeyword.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Superkeyword {
+    int age = 18; 
+
+    public static void main(String[] args) {
+        Superkeyword obj = new Superkeyword();
+        Child c = obj.new Child(); 
+        c.show();
+    }
+    class Child extends Superkeyword {
+        int age = 21;
+
+        void show() {
+            System.out.println("Child class age: " + age);
+            System.out.println("Parent class age: " + super.age);
+        }
+    }
+}
