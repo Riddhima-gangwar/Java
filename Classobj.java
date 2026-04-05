c9ffd3c2d5dd99087aaede76c40a5d4a1646aead
Riddhima Gangwar <riddhima.gangwar9@gmail.com>
2026-04-06 00:20:45 +0530

    update  commit

---
+++ Classobj.java
@@ -0,0 +1,37 @@
+public class Classobj {
+    public static void main(String[] args) {
+        Dog d1=new Dog();
+        d1.name="Tommy";
+        d1.age=5;
+
+        Dog d2=new Dog();
+        d2.name="Rex";
+        d2.age=3;
+
+        d1.walk();
+        d1.bark();
+
+        d2.walk();
+        d2.bark();
+
+    }
+}
+class Dog{
+    String name;
+    int age;
+    String color;
+
+    void walk(){
+        System.out.println(name + " is walking");
+    }
+    void bark(){
+        System.out.println(name + " is barking");
+    }
+}
+
+class Complex{
+    int a,b;
+    void print(){
+        System.out.println("The complex number is " + a + " + " + b + "i");
+    }
+}
