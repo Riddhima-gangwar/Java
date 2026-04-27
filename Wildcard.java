398917f3849d4b1fdfed75fc2a83150645734c5d
Riddhima Gangwar <riddhima.gangwar9@gmail.com>
2026-04-27 15:30:09 +0530

    update-commit

--- Wildcard.java
+++ Wildcard.java
@@ -5,28 +5,34 @@
 import java.util.*;
 
 public class Wildcard {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3));
+        PrintList(list);
+    }
+
     public void unboundedWildcard(List<?> l) {
         l.get(0);
-        //we can read but not write to the list because we don't know the type of the list
-        //l.add(1); //compile time error
+        // we can read but not write to the list because we don't know the type of the
+        // list
+        // l.add(1); //compile time error
         l.get(1);
         l.get(2);
-        }
-    
+    }
+
     public static void PrintList(List<?> l) {
         for (Object ele : l) {
             System.out.println(ele);
         }
     }
-    //array list not generic
+
+    // array list
     List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3));
+
     public void upperBoundedWildcard(List<? extends Number> l) {
-        //we can read but not write to the list because we don't know the type of the list
-        //l.add(1); //compile time error
+        // we can read but not write to the list because we don't know the type of the
+        // list
+        // l.add(1); //compile time error
         Number num = l.get(0);
         System.out.println(num);
     }
 }
-
-   
-
