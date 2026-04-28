bd02cd0e93461416ec3c66ab8f3cec3624b76743
Riddhima Gangwar <riddhima.gangwar9@gmail.com>
2026-04-28 22:00:26 +0530

    update-commit

--- Wildcard.java
+++ Wildcard.java
@@ -27,12 +27,28 @@ public class Wildcard {
 
     // array list
     List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3));
-
     public void upperBoundedWildcard(List<? extends Number> l) {
         // we can read but not write to the list because we don't know the type of the
         // list
         // l.add(1); //compile time error
         Number num = l.get(0);
         System.out.println(num);
+    }
+
+    //sum of the list of numbers
+    public static int printsum(List<? extends Number> l) {
+        int sum = 0;
+        for (Number num : l) {
+            sum += num.intValue();
+        }
+        return sum;
+    }
+
+    //lowerbound <? super Integer> or <? super Number>
+    //we can read also and write also because we know the type of the list is super of Integer or Number
+    public static void printList(List<? super Integer> l) {
+        for (Object ele : l) {
+            System.out.println(ele);
+        }
     }
 }
