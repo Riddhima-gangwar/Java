04b495d85fb6c94962455d1fd01f68d6e3074510
Riddhima Gangwar <riddhima.gangwar9@gmail.com>
2026-05-11 21:54:35 +0530

    update-commit

---
+++ Arraylist1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class Arraylist1{
+    public static void main(String[] args) {
+        // ArrayList<Integer>list = new ArrayList<>();
+        // list.add(1);
+        // list.add(3);
+        // list.add(3);
+        // list.add(6);
+
+        // for(int i=0;i<list.size();i++){
+        //     System.out.println(list.get(i));
+        // }
+
+        TreeSet<Integer>set = new TreeSet<>();
+        set.add(8);
+        set.add(4);
+        set.add(4);
+        set.add(9);
+    }
+}
