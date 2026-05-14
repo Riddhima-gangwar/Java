90ca9a0a7ae8cc5dea0a27f8b7720dd849104e83
Riddhima Gangwar <riddhima.gangwar9@gmail.com>
2026-05-14 20:10:19 +0530

    update-commit

--- Abstract.java
+++ Abstract.java
@@ -50,3 +50,21 @@ public class Abstract {
     }
 }
 
+interface A {
+    void show();
+}
+
+interface B {
+    void display();
+}
+
+class Test implements A, B {
+
+    public void show() {
+        System.out.println("Show");
+    }
+
+    public void display() {
+        System.out.println("Display");
+    }
+}
