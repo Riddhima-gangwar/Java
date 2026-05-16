6dd98985e20c129c81737f71ecec155b69d84ed5
Riddhima Gangwar <riddhima.gangwar9@gmail.com>
2026-05-16 11:56:55 +0530

    update-commit

--- Abstract.java
+++ Abstract.java
@@ -35,36 +35,102 @@
 
 
 
-abstract class A{
-    abstract void display();
-}
-class B extends A{
-    void display(){
-        System.out.println("Display method in B");
+// abstract class A{
+//     abstract void display();
+// }
+// class B extends A{
+//     void display(){
+//         System.out.println("Display method in B");
+//     }
+// }
+// public class Abstract {
+//     public static void main(String[] args) {
+//         A obj = new B();
+//         obj.display();
+//     }
+// }
+
+// interface A {
+//     void show();
+// }
+
+// interface B {
+//     void display();
+// }
+
+// class Test implements A, B {
+
+//     public void show() {
+//         System.out.println("Show");
+//     }
+
+//     public void display() {
+//         System.out.println("Display");
+//     }
+// }
+
+
+
+
+
+
+//single inheritance
+// import java.util.*;
+
+// class LPU{
+//     void animal(){
+//         System.out.println("Animal is eating");
+//     }
+// }
+
+// class Hate extends LPU{
+//     void UMS(){
+//         System.out.println("UMS is eating");
+//     }
+// }
+
+
+// public class Inheritance1 {
+//     public static void main(String args[]){
+//         Hate h= new Hate();
+//         h.animal();
+//         h.UMS();
+//     }
+// }
+
+
+
+
+// multilevel inheritance
+import java.util.*;
+
+class Animal{
+    void eat()
+    {
+        System.out.println("lpu is eating");
     }
 }
-public class Abstract {
+class Dog extends Animal{
+    void bark()
+    {
+        System.out.println("dog is barking");
+    }
+}
+class Puppy extends Dog{
+    void weep()
+    {
+        System.out.println("puppy is saying hello");
+    }
+}
+class Inheritance1{
     public static void main(String[] args) {
-        A obj = new B();
-        obj.display();
+       Puppy p=new Puppy();
+       p.bark();
+       p.eat();
+       p.weep(); 
     }
 }
 
-interface A {
-    void show();
-}
 
-interface B {
-    void display();
-}
 
-class Test implements A, B {
 
-    public void show() {
-        System.out.println("Show");
-    }
-
-    public void display() {
-        System.out.println("Display");
-    }
-}
