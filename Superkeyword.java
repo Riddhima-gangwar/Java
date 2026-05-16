cae90a66086af17bc8d2fcfdfd09fb2901bdeaca
Riddhima Gangwar <riddhima.gangwar9@gmail.com>
2026-05-16 12:22:35 +0530

    update-commit

--- Superkeyword.java
+++ Superkeyword.java
@@ -1,19 +1,45 @@
+// import java.util.*;
+
+// public class Superkeyword {
+//     int age = 18; 
+
+//     public static void main(String1[] args) {
+//         Superkeyword obj = new Superkeyword();
+//         Child c = obj.new Child(); 
+//         c.show();
+//     }
+//     class Child extends Superkeyword {
+//         int age = 21;
+
+//         void show() {
+//             System.out.println("Child class age: " + age);
+//             System.out.println("Parent class age: " + super.age);
+//         }
+//     }
+// }/
+
+
+
+
+
 import java.util.*;
-
-public class Superkeyword {
-    int age = 18; 
-
-    public static void main(String1[] args) {
-        Superkeyword obj = new Superkeyword();
-        Child c = obj.new Child(); 
-        c.show();
+class Animal{
+    void sound()
+    {
+        System.out.println("hello");
     }
-    class Child extends Superkeyword {
-        int age = 21;
-
-        void show() {
-            System.out.println("Child class age: " + age);
-            System.out.println("Parent class age: " + super.age);
-        }
+}
+class Dog extends Animal{
+    void sound()
+    {
+        System.out.println("byee");
+        super.sound();
+    }
+}
+class Superkeyword
+{
+    public static void main(String[] args) {
+        Dog d= new Dog();
+        d.sound();
     }
 }
